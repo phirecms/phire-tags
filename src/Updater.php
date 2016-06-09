86b7f751d926e9c7f9629e4f7ce9f528274c621a
Nick Sagona <nick@nolainteractive.com>
2016-06-09 10:12:35 -0500

    Add test updater

---
+++ src/Updater.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Phire Tags Module
+ *
+ * @link       https://github.com/phirecms/phire-tags
+ * @author     Nick Sagona, III <dev@nolainteractive.com>
+ * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @license    http://www.phirecms.org/license     New BSD License
+ */
+
+/**
+ * @namespace
+ */
+namespace Phire\Tags;
+
+/**
+ * Tag Updater class
+ *
+ * @category   Phire\Tags
+ * @package    Phire\Tags
+ * @author     Nick Sagona, III <dev@nolainteractive.com>
+ * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @license    http://www.phirecms.org/license     New BSD License
+ * @version    1.0.1
+ */
+class Tag extends \Phire\BaseUpdater
+{
+
+    public function update1()
+    {
+        $config = new \Phire\Table\Config([
+            'setting' => 'foo',
+            'value'   => 'bar'
+        ]);
+        $config->save();
+    }
+
+
+    public function update2()
+    {
+        $config = new \Phire\Table\Config([
+            'setting' => 'baz',
+            'value'   => '123'
+        ]);
+        $config->save();
+    }
+
+}
